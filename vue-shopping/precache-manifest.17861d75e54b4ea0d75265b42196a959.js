804f0bfc3375b236510d9e0be74cf327cb1f19ec
Thanh Chung Bui <thanhchungbtc@gmail.com>
2020-02-29 10:03:02 +0900

    add vue-shopping demo

---
+++ vue-shopping/precache-manifest.17861d75e54b4ea0d75265b42196a959.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d191a197210f8251bc12",
+    "url": "/vue-shopping/css/app.9e098776.css"
+  },
+  {
+    "revision": "684bbf42476b15ff1f8e",
+    "url": "/vue-shopping/css/chunk-vendors.4cbf3d31.css"
+  },
+  {
+    "revision": "1ac04251f6f99b462f3d505b7389760f",
+    "url": "/vue-shopping/index.html"
+  },
+  {
+    "revision": "e56141ace0ead84d6a94",
+    "url": "/vue-shopping/js/about.afc07a3c.js"
+  },
+  {
+    "revision": "d191a197210f8251bc12",
+    "url": "/vue-shopping/js/app.7d40ed34.js"
+  },
+  {
+    "revision": "684bbf42476b15ff1f8e",
+    "url": "/vue-shopping/js/chunk-vendors.6c568009.js"
+  },
+  {
+    "revision": "3b1076edf2ae4a30bd7986b4280d5099",
+    "url": "/vue-shopping/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-shopping/robots.txt"
+  }
+]);
---
+++ vue-shopping/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/vue-shopping/precache-manifest.17861d75e54b4ea0d75265b42196a959.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vue-shopping-clean-architecture"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
